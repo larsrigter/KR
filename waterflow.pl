c64f8b2acd94c6af98260e3adcafb7118fb22520
Poezedoez <raggerjonkers@gmail.com>
2019-03-15 17:34:49 +0100

    tiny steps

--- waterflow.pl
+++ waterflow.pl
@@ -3,6 +3,11 @@ quantity('inflow').
 quantity('outflow').
 quantity('volume').
 
+%% Possible derivaties
+derivative('-').
+derivative(0).
+derivative('+').
+
 %% Possible quantity spaces
 space('inflow', 0).
 space('inflow', '+').
@@ -15,6 +20,16 @@ space('volume', 0).
 space('volume', '+').
 space('volume', 'max').
 
+%% Explanations
+explain('?', 'This value is ambiguous').
+explain('-', 'This value is decreasing').
+explain('0', 'This value is constant').
+explain('+', 'This value is increasing').
+explain('max', 'This value is at its maximum').
+
+
+transition('+')
+
 %% Dependencies
 i1('inflow', 'volume').
 i0('outflow', 'volume').
@@ -22,6 +37,8 @@ p1('volume', 'outflow').
 vc('volume', 'outflow', 'max').
 vc('volume', 'outflow', 0).
 
+
+%% General (model independent) rules
 is_positively_influencing(X, Y):-
     i1(X, Y).
 
@@ -38,8 +55,7 @@ is_negatively_proportional_to(X, Y):-
     p0(Y, X).
 
 has_value_correspondence(X, Y, Value):-
-    vc(X, Y, Value);
-    vc(Y, X, Value).
+    vc(X, Y, Value).
 
 is_increasing(X):-
     is_positively_influencing(Y, X);
@@ -51,6 +67,11 @@ is_decreasing(X):-
     is_negatively_proportional_to(Y, X),
     state(Y, '+').
 
+is_decreasing(X)
+    is_positively_influencing(Y, X);
+    is_positively_proportional_to(Y, X),
+    state(Y, '-').
+
 % state(Inflow, Outflow, Volume)?
 % state('+', Outflow, Volume)?
 % -> possible states.
@@ -59,16 +80,20 @@ is_decreasing(X):-
 % state('+', '0', '+')?
 % yes.
 
+state(InflowMagnitude, OutflowMagnitude, VolumMagnitude):-
+    space('inflow', InflowMagnitude),
+    derivative(InflowDerivative),
+    space('outflow', OutflowMagnitude),
+    derivative(OutflowDerivative),
+    space('volume', VolumeMagnitude),
+    derivative(VolumeDerivative),
+    valid_state(InflowMagnitude, InflowDerivative, OutflowMagnitude, 
+        OutflowDerivative, VolumeMagnitude, VolumeDerivative).
 
-state(Inflow, Outflow, Volume):-
-    space('inflow', Inflow),
-    space('outflow', Outflow),
-    space('volume', Volume),
-    valid_state(Inflow, Outflow, Volume).
 
-
-valid_state(Inflow, Outflow, Volume):-  
-    % checking everything
+valid_state(InflowMagnitude, InflowDerivative, OutflowMagnitude, 
+    OutflowDerivative, VolumeMagnitude, VolumeDerivative):-  
+    check_conflict()
 
 
 

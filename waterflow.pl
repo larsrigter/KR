990871de9931b560567cd663caa4dfea194db13d
Poezedoez <raggerjonkers@gmail.com>
2019-04-01 18:14:06 +0200

    next_states seem legit

--- waterflow.pl
+++ waterflow.pl
@@ -3,11 +3,12 @@ quantity(inflow).
 quantity(outflow).
 quantity(volume).
 
-% Possible derivaties
-derivative_space('-').
-derivative_space(0).
-derivative_space('+').
-derivative_space('?').
+exogenous(inflow).
+
+% Possible derivatives
+derivative_space(inflow, '-').
+derivative_space(_, 0).
+derivative_space(inflow, '+').
 
 decreasing_derivative_previous('-').
 decreasing_derivative_previous(0).
@@ -16,16 +17,15 @@ increasing_derivative_previous('+').
 increasing_derivative_previous(0).
 
 % Possible quantity spaces
-quantity_space(inflow, 0).
-quantity_space(inflow, '+').
-
-quantity_space(outflow, 0).
-quantity_space(outflow, '+').
+quantity_space(_, 0).
+quantity_space(_, '+').
+quantity_space(volume, max).
 quantity_space(outflow, max).
 
-quantity_space(volume, 0).
-quantity_space(volume, '+').
-quantity_space(volume, max).
+lowest_magnitude(_, 0).
+highest_magnitude(inflow, +).
+highest_magnitude(outflow, max).
+highest_magnitude(volume, max).
 
 % Explanations
 explain('?', 'This value is ambiguous').
@@ -38,10 +38,7 @@ explain(max, 'This value is at its maximum').
 transition('-', 0).
 transition(0, '+').
 transition('+', max).
-
-is_transition(X, Y):-
-    transition(X, Y);
-    transition(Y, X).
+transition('+', '+').
 
 % Dependencies
 i1(inflow, volume).
@@ -69,6 +66,32 @@ has_value_correspondence(X, Y):-
     vc(X, Y, _);
     vc(Y, X, _).
 
+is_magnitude_transition(X, X, _):-
+    transition(X, X).
+
+is_magnitude_transition(X, X, 0).
+
+is_magnitude_transition(X, Y, Derivative):-
+    transition(X, Y),
+    Derivative == '+';
+    transition(Y, X),
+    Derivative == '-'.
+
+is_derivative_transition(Name, _, _, _):-
+    not(exogenous(Name)), !.
+
+is_derivative_transition(_, _, X, X):- !.
+
+is_derivative_transition(Name, NextMagnitude, _, 0):-
+    highest_magnitude(Name, NextMagnitude), !;
+    lowest_magnitude(Name, NextMagnitude).
+
+is_derivative_transition(Name, _, X, Y):-
+    exogenous(Name),
+    transition(X, Y), !;
+    exogenous(Name),
+    transition(Y, X).
+
 consistent_magnitude(Object, Subject):-
     Subject = [SubjectName, SubjectMagnitude, _],
     Object = [ObjectName, ObjectMagnitude, _],
@@ -80,11 +103,19 @@ consistent_magnitude(Object, Subject):-
     Object = [ObjectName, _, _],
     not(has_value_correspondence(SubjectName, ObjectName)).
 
+inconsistent_derivative([Name, Magnitude, Derivative]):-
+    Derivative == '-',
+    lowest_magnitude(Name, Magnitude), !;
+    Derivative == '+',
+    highest_magnitude(Name, Magnitude).
+
 is_increasing([SubjectName, SubjectMagnitude, SubjectDerivative], [ObjectName, _, _]):-
     is_positively_proportional_to(SubjectName, ObjectName),
     SubjectDerivative == '+';
     is_negatively_proportional_to(SubjectName, ObjectName),
     SubjectDerivative == '-';
+    is_positively_influencing(SubjectName, ObjectName),
+    SubjectMagnitude == 'max';
     is_positively_influencing(SubjectName, ObjectName),
     SubjectMagnitude == '+';
     is_negatively_influencing(SubjectName, ObjectName),
@@ -95,6 +126,8 @@ is_decreasing([SubjectName, SubjectMagnitude, SubjectDerivative], [ObjectName, _
     SubjectDerivative == '-';
     is_negatively_proportional_to(SubjectName, ObjectName),
     SubjectDerivative == '+';
+    is_negatively_influencing(SubjectName, ObjectName),
+    SubjectMagnitude == 'max';
     is_positively_influencing(SubjectName, ObjectName),
     SubjectMagnitude == '-';
     is_negatively_influencing(SubjectName, ObjectName),
@@ -108,22 +141,39 @@ is_ambiguous(Subject, Object):-
     Subject = [_, _, _],
     Object = [_, _, ObjectDerivative],
     is_increasing(Subject, Object),
-    ObjectDerivative == '-';
-    Object = [_, _, ObjectDerivative],
-    ObjectDerivative == '?'.
+    ObjectDerivative == '-'.
 
 is_steady(Subject, Object):-
-    Subject = [SubjectName, SubjectMagnitude, SubjectDerivative],
-    quantity_space(SubjectName, SubjectMagnitude),
-    derivative_space(SubjectDerivative),
-    Object = [ObjectName, ObjectMagnitude, ObjectDerivative],
-    quantity_space(ObjectName, ObjectMagnitude),
-    derivative_space(ObjectDerivative),
     not(is_decreasing(Subject, Object)),
     not(is_increasing(Subject, Object)),
     not(is_ambiguous(Subject, Object)).
 
+inequality_resolution([_, _, _], [ObjectName, ObjectMagnitude, ObjectDerivative], '-'):-
+    ObjectDerivative == '+',
+    not(inconsistent_derivative([ObjectName, ObjectMagnitude, '-'])).
+    % write("We assume the following quantity is more influential: "),
+    % write(Quantity).
 
+inequality_resolution([_, _, _], [ObjectName, ObjectMagnitude, ObjectDerivative], '-'):-
+    ObjectDerivative == '-',
+    not(inconsistent_derivative([ObjectName, ObjectMagnitude, '-'])).
+    % write("We assume the following quantity is less influential: "),
+    % write(Quantity).
+
+inequality_resolution(_, _, 0).
+    % write("We assume this quantity is just as influental as the other and they cancel each other out.").
+
+inequality_resolution([_, _, _], [ObjectName, ObjectMagnitude, ObjectDerivative], '+'):-
+    ObjectDerivative == '+',
+    not(inconsistent_derivative([ObjectName, ObjectMagnitude, '+'])).
+    % write("We assume the following quantity is less influential: "),
+    % write(Quantity).
+
+inequality_resolution([_, _, _], [ObjectName, ObjectMagnitude, ObjectDerivative], '+'):-
+    ObjectDerivative == '-',
+    not(inconsistent_derivative([ObjectName, ObjectMagnitude, '+'])).
+    % write("We assume the following quantity is more influential: "),
+    % write(Quantity).
 
 derivative_calculus(Subject, Object, [ObjectName, ObjectMagnitude, '+']):-
     Object = [ObjectName, ObjectMagnitude, ObjectDerivative],
@@ -135,82 +185,83 @@ derivative_calculus(Subject, Object, [ObjectName, ObjectMagnitude, '-']):-
     decreasing_derivative_previous(ObjectDerivative),
     is_decreasing(Subject, Object).
 
-derivative_calculus(Subject, Object, [ObjectName, ObjectMagnitude, '?']):-
+derivative_calculus(Subject, Object, [ObjectName, ObjectMagnitude, Inequality]):-
     Object = [ObjectName, ObjectMagnitude, _],
-    is_ambiguous(Subject, Object).
+    is_ambiguous(Subject, Object), !,
+    inequality_resolution(Subject, Object, Inequality).
 
 derivative_calculus(Subject, Object, [ObjectName, ObjectMagnitude, ObjectDerivative]):-
     Object = [ObjectName, ObjectMagnitude, ObjectDerivative],
     consistent_magnitude(Subject, Object),
-    is_steady(Subject, Object).
+    is_steady(Subject, Object), !.
 
-resolution_single(Subject, [Object], [ResolvedObject]):-
-    consistent_magnitude(Subject, Object),
-    derivative_calculus(Subject, Object, ResolvedObject), !.
-
+resolution_single(_, [], []):- !.
 resolution_single(Subject, [Object|Rest], [ResolvedObject|ResolvedObjects]):-
-    consistent_magnitude(Subject, Object),
+    consistent_magnitude(Subject, Object), !,
     derivative_calculus(Subject, Object, ResolvedObject),
     resolution_single(Subject, Rest, ResolvedObjects).
 
-resolution([SubjectState], ObjectStates, ResultStates):-
-    resolution_single(SubjectState, ObjectStates, ResultStates), !.
+resolution([], State, State):- !.
+resolution(SubjectState, ObjectState, ResultState):-
+    SubjectState = [Q|T],
+    resolution_single(Q, ObjectState, NextState),
+    length(T, TailLength),
+    length(ObjectState, StateLength),
+    Pop = StateLength-TailLength,
+    split(NextState, Pop, _, L2),
+    resolution(L2, NextState, ResultState).
 
-resolution(SubjectStates, ObjectStates, ResultStates):-
-    SubjectStates = [S|T],
-    % S = [SubjectName, SubjectMagnitude, SubjectDerivative],
-    % quantity_space(SubjectName, SubjectMagnitude),
-    % derivative_space(SubjectDerivative),
-    resolution_single(S, ObjectStates, NextStates),
-    resolution(T, NextStates, ResultStates).
+% ASSUMPTION: Derivative cannot be negative if magnitude is at lowest point!
+% ASSUMPTION: Derivative cannot be positive if magnitude is at highest point!
 
+% Pick a state according to the possible values each quantity can take on
+pick_state([], []):- !.
+pick_state([Quantity|T], [Quantity|PickedQuantities]):-
+    Quantity = [Name, Magnitude, Derivative],
+    quantity_space(Name, Magnitude),
+    derivative_space(Name, Derivative),
+    not(inconsistent_derivative(Quantity)),
+    pick_state(T, PickedQuantities).
 
+% Pick a state preserving the order of names to reduce total combinations
+pick_name_state([], []):- !.
+pick_name_state([[Name, _, _]|Quantities], [[Name, _, _]|NextQuantities]):-
+    pick_name_state(Quantities, NextQuantities).
 
-% state([['inflow', 0, ''+''], ['outflow', M1, D1], ['volume', M2, D2]])
+% Pick a state which complies to the transition rules
+transition_state(State, TransitionState):-
+    pick_name_state(State, PreservedNames),
+    pick_state(PreservedNames, TransitionState),
+    valid_transition(State, TransitionState).
 
-% state([]).
-% state(State):-
-%     State = [Quantity, Magnitude, Derivative],
-%     quantity_space(Quantity, Magnitude),
-%     derivative_space(Derivative).
+next_states(State, NextStates):-
+    setof(NextState, next_state(State, NextState), NextStates).
 
-% state(States):-
-%     States = [S|T],
-%     S = [Quantity, Magnitude, Derivative],
-%     quantity_space(Quantity, Magnitude),
-%     derivative_space(Derivative),
-%     state(T).
+% Pick a next transition state subject to the transition rules
+next_state(State, NextState):-
+    transition_state(State, TransitionState),
+    resolution(TransitionState, TransitionState, NextState).
 
+% Check for each quantity if its next values are legit
+valid_transition([], []):- !.
+valid_transition([Quantity|Quantities], [NextQuantity|NextQuantities]):-
+    Quantity = [Name, Magnitude, Derivative],
+    NextQuantity = [_, NextMagnitude, NextDerivative],
+    is_magnitude_transition(Magnitude, NextMagnitude, Derivative), !,
+    is_derivative_transition(Name, NextMagnitude, Derivative, NextDerivative), !,
+    valid_transition(Quantities, NextQuantities).
 
-pick_states([[InitialName, _, _]], ObjectState):-
-    ObjectState = [InitialName, PrevMagnitude, PrevDerivative],
-    quantity_space(InitialName, PrevMagnitude),
-    derivative_space(PrevDerivative).
+% Print function: print a nested list in a readable manner
+print([]).
+print([H|T]):-
+    write(H),
+    nl,
+    print(T).
 
-pick_states([H|T], [ObjectState|ObjectStates]):-
-    H = [InitialName, _, _],
-    ObjectState = [InitialName, PrevMagnitude, PrevDerivative],
-    quantity_space(InitialName, PrevMagnitude),
-    derivative_space(PrevDerivative),
-    pick_states(T, ObjectStates).
-
-state(States):-
-    % pick_states(States, PickedObjectStates),
-    pick_states(States, PickedStates),
-    resolution(PickedStates, PickedStates, States).
-
-% state([SubjectState], ObjectStates, ResultStates):-
-%     resolution(SubjectState, ObjectStates, ResultStates).
-
-% state(SubjectStates, ObjectStates, ResultStates):-
-%     SubjectStates = [S|T],
-%     % S = [SubjectName, SubjectMagnitude, SubjectDerivative],
-%     % quantity_space(SubjectName, SubjectMagnitude),
-%     % derivative_space(SubjectDerivative),
-%     resolution(S, ObjectStates, NextStates),
-%     state(T, NextStates, ResultStates).
-
-
-
-
+% Split a list into two sublist given the length of the first list
+split(L, 0, [], L).
+split([H|T], N, [H|X], L2) :-
+        N > 0,
+        M is N - 1,
+        split(T, M, X, L2).
 

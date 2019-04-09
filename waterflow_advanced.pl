8ab9b946a73938e39faf6936852a136c6cdc72bf
Poezedoez <raggerjonkers@gmail.com>
2019-04-09 13:06:18 +0200

    probably final prolog versions

--- waterflow_advanced.pl
+++ waterflow_advanced.pl
@@ -2,6 +2,8 @@
 quantity(inflow).
 quantity(outflow).
 quantity(volume).
+quantity(pressure).
+quantity(height).
 
 exogenous(inflow).
 
@@ -21,19 +23,15 @@ quantity_space(_, 0).
 quantity_space(_, '+').
 quantity_space(volume, max).
 quantity_space(outflow, max).
+quantity_space(pressure, max).
+quantity_space(height, max).
 
 lowest_magnitude(_, 0).
 highest_magnitude(inflow, +).
 highest_magnitude(outflow, max).
 highest_magnitude(volume, max).
-
-% Explanations
-explain(quantity, magnitude, explanation).
-explain('?', 'This value is ambiguous').
-explain('-', 'This value is decreasing').
-explain(0, 'This value is constant').
-explain('+', 'This value is increasing').
-explain(max, 'This value is at its maximum').
+highest_magnitude(pressure, max).
+highest_magnitude(height, max).
 
 % Transitions
 transition('-', 0).
@@ -44,10 +42,24 @@ transition('+', '+').
 % Dependencies
 i1(inflow, volume).
 i0(outflow, volume).
-p1(volume, outflow).
+p1(pressure, outflow).
+p1(volume, height).
+p1(height, pressure).
 p0(something, somethingelse).
-vc(volume, outflow, max).
-vc(volume, outflow, 0).
+
+vc(Q1, Q2, max):-
+    quantity(Q1),
+    quantity(Q2),
+    Q1 \= inflow,
+    Q2 \= inflow,
+    Q1 \= Q2.
+
+vc(Q1, Q2, 0):-
+    quantity(Q1),
+    quantity(Q2),
+    Q1 \= inflow,
+    Q2 \= inflow,
+    Q1 \= Q2.
 
 % General (model independent) rules
 is_positively_influencing(X, Y):-
